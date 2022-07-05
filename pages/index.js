cd22755091a600a049cdbd1839a7c9a9befdc343
Nat Ring <natring@rocketinsights.com>
2022-07-05 13:45:55 -0400

    Get Auth0 POC working

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import styles from '../styles/Home.module.css'
+import { useUser } from '@auth0/nextjs-auth0'
 
 export default function Home() {
+  const { user, error, isLoading } = useUser()
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+
   return (
     <div className={styles.container}>
-      Shoutout!
+      <h1>Shoutout!</h1>
+
+      {user ? <p>{user.name} {user.email}</p> : (
+        <ul>
+          <li><a href="/api/auth/login">Login</a></li>
+          <li><a href="/api/auth/logout">Logout</a></li>
+        </ul>
+      )}
     </div>
   )
 }
