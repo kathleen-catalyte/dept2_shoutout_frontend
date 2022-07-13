4c54892cbc6e871917524bd624c22e81f208dd36
Nat Ring <natring@rocketinsights.com>
2022-07-13 14:44:47 -0400

    Prefer error.toString() to print error name and error message
    Co-authored-by: Danny (he/him) <DanielKim1@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { useUser } from '@auth0/nextjs-auth0'
 export default function Home() {
   const { user, error, isLoading } = useUser()
   if (isLoading) return <div>Loading...</div>
-  if (error) return <div>{error.message}</div>
+  if (error) return <div>{error.toString()}</div>
 
   return (
     <div className={styles.container}>
