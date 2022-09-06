37fa2a9ab17213eab5a9f18722fe7b4844830358
Nat Ring <natring@rocketinsights.com>
2022-09-06 15:21:38 -0400

    Stop trying to make fetch happen

--- components/shoutouts-temp/index.tsx
+++ components/shoutouts-temp/index.tsx
@@ -1,9 +1,8 @@
 import useSWR from 'swr'
-
-const fetcher = (...args: any) => fetch(...args).then((res) => res.json())
+import fetch from '../../lib/fetch'
 
 const ShoutoutsTemp = () => {
-  const { data, error } = useSWR('/api/shoutouts/latest', fetcher)
+  const { data, error } = useSWR('/api/shoutouts/latest', fetch)
 
   if (error) return <p>Something went wrong.</p>
   if (!data) return <p>Hang tight...</p>
