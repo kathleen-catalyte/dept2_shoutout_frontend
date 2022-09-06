91f14144ac3a4e16476c4e35a7a6f7d584d83f94
Nat Ring <natring@rocketinsights.com>
2022-09-06 15:33:57 -0400

    Remove unused example 'hello' api route. Add an alias to make referencing components more consistent

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import { useUser } from '@auth0/nextjs-auth0'
 import type { NextPage } from 'next'
-import ShoutoutsTemp from '../components/shoutouts-temp'
+import ShoutoutsTemp from '@/components/shoutouts-temp'
 
-import styles from '../styles/Home.module.css'
+import styles from '@/styles/Home.module.css'
 
 const Home: NextPage = () => {
   const { user, error, isLoading } = useUser()
