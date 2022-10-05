b65932a098f6d2c4a29e0490076040615ca5a418
Alberto Gonzalez <91083483+agonxalez@users.noreply.github.com>
2022-10-05 11:50:34 -0500

    added mocking data functionality (#1)
    * added mocking data functionality
    
    * remove req, and added line to end of file.
    
    * Added Req back
    
    Co-authored-by: Alberto Gonzalez <agonzalez@catalyte.io>

--- pages/api/shoutouts/latest.ts
+++ pages/api/shoutouts/latest.ts
@@ -2,19 +2,31 @@
 import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+
 export default withApiAuthRequired(async function latestShoutouts(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { accessToken } = await getAccessToken(req, res, {});
-  const url = `${process.env.API_HOST}/shoutouts/latest`
+  let latest;
+  if (process.env.API_HOST) {
+    const { accessToken } = await getAccessToken(req, res, {});
+    const url = `${process.env.API_HOST}/shoutouts/latest`
 
-  const response = await fetch(url, {
-    headers: {
-      authorization: `Bearer ${accessToken}`
-    }
-  });
+    const response = await fetch(url, {
+      headers: {
+        authorization: `Bearer ${accessToken}`
+      }
+    });
 
-  const latest = await response.json();
+    latest = await response.json();
+  } else {
+    // use mock data if no api_host provided
+    console.warn("!!!!! No API_HOST configured using mock data !!!!!")
+    const url = `${process.env.DEV_API_URL}/latest`
+    const response = await fetch(url);
+
+    latest = await response.json();
+  }
+
   res.status(200).json(latest);
 });
