87e8dc0d6a050f192b11798d7a36cf4b23f33683
Brian Durel <bdurel@catalyte.io>
2022-10-06 12:20:48 -0400

    adds search icon and removes package-lock

--- components/search/search-box.tsx
+++ components/search/search-box.tsx
@@ -1,3 +1,6 @@
+import MagnifyingGlass from "heroicons/20/solid/magnifying-glass.svg";
+import Image from "next/image";
+
 import styles from "./search-box.module.css";
 
 // setstate of input
@@ -5,9 +8,16 @@ import styles from "./search-box.module.css";
 // on click of x set textfield state to empty string
 
 const SearchBox = () => {
+  console.log(MagnifyingGlass);
+
   return (
     <div className={styles.searchWrapper}>
       <form>
+        <Image
+          src={MagnifyingGlass}
+          alt=""
+          className={styles.magnifyingGlassInactive}
+        ></Image>
         <input type="text" className={styles.searchBox} />
         {/* add the magnifying icon here */}
         <button className={styles.closeIcon} type="reset" />
