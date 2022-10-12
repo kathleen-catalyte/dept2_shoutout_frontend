ad0f687e9f67f56870f6c16b23df246c34e83938
Brian Durel <bdurel@catalyte.io>
2022-10-12 19:18:03 -0400

    adds case insensitivity and caps results at first 100

--- pages/api/dev/profile/search.ts
+++ pages/api/dev/profile/search.ts
@@ -14,8 +14,8 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
   // create an array of data based on Nat's
   const dataSet = shoutouts;
   // grab the query from the url
-  const queriedName: string = req.query.name as string;
-  const queriedEmail: string = req.query.email as string;
+  const queriedName: string = (req.query.name as string).toLowerCase();
+  const queriedEmail: string = (req.query.email as string).toLowerCase();
   // create response array to add matches to
   let responseArray: BasicProfile[] = [];
   // loop through the array to see if it has possible matches
@@ -24,21 +24,27 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
 
     recipientsDataSet.forEach((recipient) => {
       if (
-        (recipient.name.includes(queriedName) ||
+        (recipient.name.toLowerCase().includes(queriedName) ||
           recipient.email.includes(queriedEmail)) &&
         !responseArray.some((profile) => profile.name === recipient.name)
       ) {
         // push matching recipient to the responseArray
         responseArray.push(recipient);
+        if (responseArray.length >= 101) {
+          return res.status(200).json(responseArray);
+        }
       }
     });
 
     if (
-      shoutout.author.name.includes(queriedName) ||
+      shoutout.author.name.toLowerCase().includes(queriedName) ||
       (shoutout.author.email.includes(queriedEmail) &&
         !responseArray.some((profile) => profile.name === shoutout.author.name))
     ) {
       responseArray.push(shoutout.author);
+      if (responseArray.length >= 101) {
+        return res.status(200).json(responseArray);
+      }
     }
   });
 
