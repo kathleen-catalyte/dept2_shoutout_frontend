c733361186be7ebaadd70f15b0d74b654a648eb2
Benjamin Lamboy <blamboy@catalyte.io>
2022-10-13 12:58:05 -0600

    added fonts, fixed stylesheet location for shoutouts, made each shoutout its own component, updated shoutout container color

--- components/shoutouts/index.tsx
+++ components/shoutouts/index.tsx
@@ -2,7 +2,8 @@ import { Element, Shoutout } from "src/ts/interfaces/shoutout";
 import useSWR from "swr";
 
 import fetch from "@/lib/fetch";
-import styles from "@/styles/Shoutouts.module.css";
+
+import ShoutoutComp from "./shoutoutcomp";
 
 const Shoutouts = () => {
   const { data, error } = useSWR("/api/shoutouts/latest", fetch);
@@ -10,23 +11,12 @@ const Shoutouts = () => {
 
   if (error) return <p>Something went wrong.</p>;
   if (!data) return <p>Hang tight...</p>;
-  console.log(shoutOutData.length);
+
   return (
     <div>
       {shoutOutData.map(function (d, idx) {
-        return (
-          <div className={styles.shoutcontainer} key={idx}>
-            <p className={styles.text}>
-              {d.elements.map((x, idx) => {
-                return <a key={idx}>{` ${x.text}`}</a>;
-              })}
-            </p>
-            <span className={styles.userName}>{`@${d.author.name}`}</span>
-            <span className={styles.subtext}>{`#${d.channel.id}`}</span>
-          </div>
-        );
+        return <ShoutoutComp shoutout={d} key={idx} />;
       })}
-      
     </div>
   );
 };
