e35bfb992158f180f9445e4561b75a3b02fc8feb
Alberto Gonzalez <91083483+agonxalez@users.noreply.github.com>
2022-10-14 10:24:17 -0500

    Dept2 12 (#3)
    * created interface for shoutout object
    
    * added styling to landing page
    
    * updated styling centered shoutouts added icon at top of page
    
    * Delete package-lock.json
    
    * corrected shoutout content to use elements updated various style elements
    
    * remove package lock
    
    * added grid to shoutout
    
    * updated styling and rendered more sample shoutouts
    
    * updated styling to have container flex, removed placeholder items
    
    * added jest library
    
    * added fonts, fixed stylesheet location for shoutouts, made each shoutout its own component, updated shoutout container color
    
    * Updated styling to better match figma mock
    
    * added more spacing between heading and icon
    
    * corrected spacing
    
    Co-authored-by: Benjamin Lamboy <blamboy@catalyte.io>
    Co-authored-by: bdurel <114426964+bdurel@users.noreply.github.com>

--- pages/api/dev/latest.ts
+++ pages/api/dev/latest.ts
@@ -1,17 +1,15 @@
-import { faker } from '@faker-js/faker';
-import { NextApiResponse } from "next"
+import { faker } from "@faker-js/faker";
+import { NextApiResponse } from "next";
 
-
-const handler = ( req: NextApiResponse, res: NextApiResponse) => {
-  res
-    .status(200)
-    .json([
-      {
-        id: 1,
-        text: "hi <@Beto> ",
-        createDate: faker.date.soon(),
-        authorId: "1",
-        recipients: [{
+const handler = (req: NextApiResponse, res: NextApiResponse) => {
+  res.status(200).json([
+    {
+      id: 1,
+      text: "hi. <@Beto> " + faker.lorem.sentence(),
+      createDate: faker.date.soon(),
+      authorId: "1",
+      recipients: [
+        {
           employeeId: "2",
           email: faker.internet.email(),
           team: "DPUS",
@@ -19,38 +17,229 @@ const handler = ( req: NextApiResponse, res: NextApiResponse) => {
           name: "Beto",
           image72: faker.image.avatar(),
           image192: faker.image.avatar(),
-          image512: faker.image.avatar()
-        }],
-        elements:
-          [{
-            id: 1,
-            content: "hi",
-            type: "text",
-            employeeId: null
-          }, {
-            id: 2,
-            content: "Beto",
-            type: "user",
-            employeeId: "2"
-          }]
-        ,
-        channel: {
-          id: faker.lorem.sentence(),
-          slackId: faker.datatype.uuid(),
-          name: faker.name.firstName()
+          image512: faker.image.avatar(),
         },
-        author: {
-          employeeId: 1,
+      ],
+      elements: [
+        {
+          id: 1,
+          text: "hi.",
+          type: "text",
+          employeeId: null,
+        },
+        {
+          id: 2,
+          text: "Beto",
+          type: "user",
+          employeeId: "2",
+        },
+        { id: 3, text: faker.lorem.paragraph(), type: "text" },
+      ],
+      channel: {
+        id: faker.lorem.sentence(),
+        slackId: faker.datatype.uuid(),
+        name: faker.name.firstName(),
+      },
+      author: {
+        employeeId: 1,
+        email: faker.internet.email(),
+        team: "DPUS",
+        country: "US",
+        name: "Chuck",
+        image72: faker.image.avatar(),
+        image192: faker.image.avatar(),
+        image512: faker.image.avatar(),
+      },
+    },
+    {
+      id: 3,
+      text: "hi. <@Beto> ",
+      createDate: faker.date.soon(),
+      authorId: "1",
+      recipients: [
+        {
+          employeeId: "2",
           email: faker.internet.email(),
           team: "DPUS",
           country: "US",
-          name: "Shouty",
+          name: "Beto",
           image72: faker.image.avatar(),
           image192: faker.image.avatar(),
-          image512: faker.image.avatar()
-        }
-      }
-    ])
-}
+          image512: faker.image.avatar(),
+        },
+      ],
+      elements: [
+        {
+          id: 1,
+          text: "hi",
+          type: "text",
+          employeeId: null,
+        },
+        {
+          id: 2,
+          text: "Beto",
+          type: "user",
+          employeeId: "2",
+        },
+      ],
+      channel: {
+        id: faker.lorem.sentence(),
+        slackId: faker.datatype.uuid(),
+        name: faker.name.firstName(),
+      },
+      author: {
+        employeeId: 1,
+        email: faker.internet.email(),
+        team: "DPUS",
+        country: "US",
+        name: "Annabelle",
+        image72: faker.image.avatar(),
+        image192: faker.image.avatar(),
+        image512: faker.image.avatar(),
+      },
+    },
+    {
+      id: 1,
+      text: "hi. <@Beto> ",
+      createDate: faker.date.soon(),
+      authorId: "1",
+      recipients: [
+        {
+          employeeId: "2",
+          email: faker.internet.email(),
+          team: "DPUS",
+          country: "US",
+          name: "Beto",
+          image72: faker.image.avatar(),
+          image192: faker.image.avatar(),
+          image512: faker.image.avatar(),
+        },
+      ],
+      elements: [
+        {
+          id: 1,
+          text: "hi",
+          type: "text",
+          employeeId: null,
+        },
+        {
+          id: 2,
+          text: "Beto",
+          type: "user",
+          employeeId: "2",
+        },
+      ],
+      channel: {
+        id: faker.lorem.sentence(),
+        slackId: faker.datatype.uuid(),
+        name: faker.name.firstName(),
+      },
+      author: {
+        employeeId: 1,
+        email: faker.internet.email(),
+        team: "DPUS",
+        country: "US",
+        name: "Shouty",
+        image72: faker.image.avatar(),
+        image192: faker.image.avatar(),
+        image512: faker.image.avatar(),
+      },
+    },{
+      id: 1,
+      text: "hi. <@Beto> " + faker.lorem.sentence(),
+      createDate: faker.date.soon(),
+      authorId: "1",
+      recipients: [
+        {
+          employeeId: "2",
+          email: faker.internet.email(),
+          team: "DPUS",
+          country: "US",
+          name: "Beto",
+          image72: faker.image.avatar(),
+          image192: faker.image.avatar(),
+          image512: faker.image.avatar(),
+        },
+      ],
+      elements: [
+        {
+          id: 1,
+          text: "hi.",
+          type: "text",
+          employeeId: null,
+        },
+        {
+          id: 2,
+          text: "Beto",
+          type: "user",
+          employeeId: "2",
+        },
+        { id: 3, text: faker.lorem.paragraph(), type: "text" },
+      ],
+      channel: {
+        id: faker.lorem.sentence(),
+        slackId: faker.datatype.uuid(),
+        name: faker.name.firstName(),
+      },
+      author: {
+        employeeId: 1,
+        email: faker.internet.email(),
+        team: "DPUS",
+        country: "US",
+        name: "Chuck",
+        image72: faker.image.avatar(),
+        image192: faker.image.avatar(),
+        image512: faker.image.avatar(),
+      },
+    },{
+      id: 1,
+      text: "hi. <@Beto> " + faker.lorem.sentence(),
+      createDate: faker.date.soon(),
+      authorId: "1",
+      recipients: [
+        {
+          employeeId: "2",
+          email: faker.internet.email(),
+          team: "DPUS",
+          country: "US",
+          name: "Beto",
+          image72: faker.image.avatar(),
+          image192: faker.image.avatar(),
+          image512: faker.image.avatar(),
+        },
+      ],
+      elements: [
+        {
+          id: 1,
+          text: "hi.",
+          type: "text",
+          employeeId: null,
+        },
+        {
+          id: 2,
+          text: "Beto",
+          type: "user",
+          employeeId: "2",
+        },
+        { id: 3, text: faker.lorem.paragraph(), type: "text" },
+      ],
+      channel: {
+        id: faker.lorem.sentence(),
+        slackId: faker.datatype.uuid(),
+        name: faker.name.firstName(),
+      },
+      author: {
+        employeeId: 1,
+        email: faker.internet.email(),
+        team: "DPUS",
+        country: "US",
+        name: "Chuck",
+        image72: faker.image.avatar(),
+        image192: faker.image.avatar(),
+        image512: faker.image.avatar(),
+      },
+    },
+  ]);
+};
 
-export default handler
+export default handler;
