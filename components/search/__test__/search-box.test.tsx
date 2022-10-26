10b49a1f7bd0dcfd6497d559e31dcfc17901bdd2
Brian Durel <bdurel@catalyte.io>
2022-10-25 22:04:17 -0400

    removes commented out code and all tests pass

--- components/search/__test__/search-box.test.tsx
+++ components/search/__test__/search-box.test.tsx
@@ -15,45 +15,7 @@ global.fetch = jest.fn(() =>
   })
 ) as jest.Mock;
 
-//Mocking some used Auth0 methods to bypass Auth0.
-// jest.mock('@auth0/nextjs-auth0', () => ({
-//   withApiAuthRequired: jest
-//     .fn()
-//     .mockImplementation((component, ignore) => component),
-//   getAccessToken: jest
-//     .fn()
-//     .mockImplementation((component, ignore) => component),
-// }));
-
-// const localStorageMock = (() => {
-//   let store: any = {};
-
-//   return {
-//     getItem(key: string | number) {
-//       return store[key] || null;
-//     },
-//     setItem(key: string | number, value: { toString: () => any }) {
-//       store[key] = value.toString();
-//     },
-//     removeItem(key: string | number) {
-//       delete store[key];
-//     },
-//     clear() {
-//       store = {};
-//     },
-//   };
-// })();
-
-// Object.defineProperty(window, 'sessionStorage', {
-//   value: localStorageMock,
-// });
-
 describe('SearchBox', () => {
-  beforeEach(() => {
-    window.sessionStorage.clear();
-    jest.restoreAllMocks();
-  });
-
   it('should render inactive search box with inactive search icon, placeholder text and disabled submit button when component initially loads', () => {
     render(<SearchBox childToParent={childToParent} />);
 
@@ -105,46 +67,9 @@ describe('SearchBox', () => {
     await userEvent.type(screen.getByRole('text'), 'a');
     await userEvent.click(screen.getByText('Search'));
 
-    // const getItemSpy = jest.spyOn(window.sessionStorage, 'getItem');
-    // window.sessionStorage.setItem(
-    //   'profileSearchResults',
-    //   JSON.stringify([
-    //     {
-    //       employeeId: '1',
-    //       email: 'X_MAyra_Hudson@gmail.com',
-    //       team: 'DPUS',
-    //       country: 'US',
-    //       name: 'n/a',
-    //       image72:
-    //         'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1.jpg',
-    //       image192:
-    //         'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/2.jpg',
-    //       image512:
-    //         'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/3.jpg',
-    //     },
-    //     {
-    //       employeeId: '2',
-    //       email: 'Mary_Hudson@gmail.com',
-    //       team: 'DPUS',
-    //       country: 'US',
-    //       name: 'n/a',
-    //       image72:
-    //         'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1.jpg',
-    //       image192:
-    //         'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/2.jpg',
-    //       image512:
-    //         'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/3.jpg',
-    //     },
-    //   ])
-    // );
-
-    // const actualValue =
-    ////////////////////////////
     const storedSearchResults: BasicProfile[] = JSON.parse(
       sessionStorage.getItem('profileSearchResults') as string
     );
-
-    console.log(storedSearchResults);
 
     expect(
       storedSearchResults.find((profile) => profile.employeeId === '1')
