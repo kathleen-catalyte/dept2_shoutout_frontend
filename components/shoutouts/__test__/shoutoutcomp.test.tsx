db0386f8508ae5a962c87b4f2d8b44b638aecda3
Benjamin Lamboy <blamboy@catalyte.io>
2022-10-26 13:56:12 -0600

    refactor test

--- components/shoutouts/__test__/shoutoutcomp.test.tsx
+++ components/shoutouts/__test__/shoutoutcomp.test.tsx
@@ -1,20 +1,13 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from "@testing-library/react";
 
-import ShoutoutComp from '../shoutoutcomp';
+import ShoutoutComp from "../shoutoutcomp";
 
-const mockShoutout = require('./mockShoutout.json')
+const mockShoutout = require("./mockShoutout.json");
 
-// TEST BEHAVIOUR, NOT THE IMPLEMENTATION
-// Test that see-shoutouts button takes employeeId of search componenet and navigates to profile page
-// Click see-shoutout button on search component and user is navigated to employee profile page
+describe("Shoutout Component", () => {
+  test("renders shoutout component", () => {
+    render(<ShoutoutComp shoutout={mockShoutout} />);
 
-test('renders see-shoutout component', () => {
-    // 1) rendering the component we want to test
-    render(<ShoutoutComp shoutout={ mockShoutout} />)
-    
-    // 2) Finding the elements
-    const shoutoutElement = screen.getByTestId('custom-element');
-
-    // 3) Assertion
-    expect(shoutoutElement).toBeInTheDocument();
-})
+    expect(screen.getByTestId("custom-element")).toBeInTheDocument();
+  });
+});
