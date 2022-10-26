1c6221994797a50209323981b2ff17c495052c5a
Brian Durel <bdurel@catalyte.io>
2022-10-26 15:21:01 -0400

    working stack of icons adjusted for styling but with gap between image and image border

--- components/shoutouts/shoutout-comp.tsx
+++ components/shoutouts/shoutout-comp.tsx
@@ -8,24 +8,36 @@ import styles from './Shoutouts.module.css';
 const ShoutoutComp = (props: { shoutout: Shoutout }, key: number) => {
   const { shoutout } = props;
   return (
-    <div className={styles.shoutcontainer}>
-      <p className={styles.text}>
-        {shoutout.elements?.map(
-          (x: Element, idx: React.Key | null | undefined) => {
-            return x.employeeId ? (
-              <Link key={idx} href={`/user/${x.employeeId}`}>
-                <a className={styles.linkToProfile}>{` @${x.text} `}</a>
-              </Link>
-            ) : (
-              <a key={idx}>{` ${x.text}`}</a>
-            );
-          }
-        )}
-      </p>
-      <Link href={`/user/${shoutout.author.employeeId}`}>
-        <a className={styles.userName}>{`@${shoutout.author?.name}`}</a>
-      </Link>
-      <span className={styles.subtext}>{`#${shoutout.channel?.id}`}</span>
+    <div>
+      {' '}
+      <div className={styles.avatars}>
+        {shoutout.recipients?.map((recipient) => (
+          <span key={recipient.employeeId} className={styles.avatar}>
+            <ProfilePicture
+              picture={recipient.image192 ? recipient.image192 : ''}
+            />
+          </span>
+        ))}
+      </div>
+      <div className={styles.shoutcontainer}>
+        <p className={styles.text}>
+          {shoutout.elements?.map(
+            (x: Element, idx: React.Key | null | undefined) => {
+              return x.employeeId ? (
+                <Link key={idx} href={`/user/${x.employeeId}`}>
+                  <a className={styles.linkToProfile}>{` @${x.text} `}</a>
+                </Link>
+              ) : (
+                <a key={idx}>{` ${x.text}`}</a>
+              );
+            }
+          )}
+        </p>
+        <Link href={`/user/${shoutout.author.employeeId}`}>
+          <a className={styles.userName}>{`@${shoutout.author?.name}`}</a>
+        </Link>
+        <span className={styles.subtext}>{`#${shoutout.channel?.id}`}</span>
+      </div>
     </div>
   );
 };
