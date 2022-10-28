cbdc9b353f75de268a8b5493a023b27456be75d3
bdurel <114426964+bdurel@users.noreply.github.com>
2022-10-28 16:39:11 -0400

    Dept2 28 user icon on search results (#21)
    * fixes text overflow by adding padding to input
    
    * fixes placeholder text
    
    * renames files to be consistant with the rest of the project
    
    * correctly styles avatar icons on search results
    
    * adds in placeholder avatar for null images
    
    * removes hidden avatar class
    
    Co-authored-by: Brian Durel <bdurel@catalyte.io>

--- components/search-result/search-result-box.tsx
+++ components/search-result/search-result-box.tsx
@@ -1,13 +1,19 @@
-import React from "react";
-import { BasicProfile } from "ts/interfaces";
+import defaultAvatar from 'images/defaultAvatar.webp';
+import React from 'react';
+import { BasicProfile } from 'ts/interfaces';
 
-import SeeShoutouts from "../see-shoutouts/see-shoutouts";
-import styles from "./SearchBox.module.css";
+import ProfilePicture from '../profile-picture';
+import SeeShoutouts from '../see-shoutouts/see-shoutouts';
+import styles from './SearchResultBox.module.css';
 
 const SearchResultBox = (userData: { data: BasicProfile }) => {
   const { data } = userData;
+
   return (
     <div className={styles.container}>
+      <div className={styles.avatar}>
+        <ProfilePicture picture={data.image72 ? data.image72 : defaultAvatar} />
+      </div>
       <a className={styles.name}>@{data?.name}</a>
       <div className={styles.btn}>
         <SeeShoutouts employeeId={data.employeeId} />
