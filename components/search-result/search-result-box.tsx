2ca65874e9c67dbc2913ce8673c21f742b36c7ef
Brian Durel <bdurel@catalyte.io>
2022-10-28 12:18:59 -0400

    renames files to be consistant with the rest of the project

--- components/search-result/search-result-box.tsx
+++ components/search-result/search-result-box.tsx
@@ -1,13 +1,17 @@
-import React from "react";
-import { BasicProfile } from "ts/interfaces";
+import React from 'react';
+import { BasicProfile } from 'ts/interfaces';
 
-import SeeShoutouts from "../see-shoutouts/see-shoutouts";
-import styles from "./SearchBox.module.css";
+import ProfilePicture from '../profile-picture';
+import SeeShoutouts from '../see-shoutouts/see-shoutouts';
+import styles from './SearchResultBox.module.css';
 
 const SearchResultBox = (userData: { data: BasicProfile }) => {
   const { data } = userData;
   return (
     <div className={styles.container}>
+      {/* <div>
+        <ProfilePicture picture={data.image72 ? data.image72 : ''} />
+      </div> */}
       <a className={styles.name}>@{data?.name}</a>
       <div className={styles.btn}>
         <SeeShoutouts employeeId={data.employeeId} />
--- components/search-result/search-result.tsx
+++ components/search-result/search-result.tsx
@@ -1,48 +1,61 @@
-import { XMarkIcon } from '@heroicons/react/24/solid'
-import Router from 'next/router'
-import React, { useEffect, useState } from 'react'
-import { BasicProfile } from 'ts/interfaces'
+import { XMarkIcon } from '@heroicons/react/24/solid';
+import Router from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { BasicProfile } from 'ts/interfaces';
 
-import SearchResultBox from './search-result-box'
-import styles from './SearchBoxContainer.module.css'
+import SearchResultBox from './search-result-box';
+import styles from './SearchResult.module.css';
 
-const SearchResult = ({ childToParent }: { childToParent: (childdata: boolean) => void }) => {
-    const [storedSearchResults, setStoredSearchResults] = useState<BasicProfile[]>([]);
+const SearchResult = ({
+  childToParent,
+}: {
+  childToParent: (childdata: boolean) => void;
+}) => {
+  const [storedSearchResults, setStoredSearchResults] = useState<
+    BasicProfile[]
+  >([]);
 
-    useEffect(() => {
-        const storedSearchResults = sessionStorage.getItem('profileSearchResults');
-        if (storedSearchResults) {
-            setStoredSearchResults(JSON.parse(storedSearchResults));
-        }
-    }, [])
-    const ClearSearchResults = () => {
-        sessionStorage.clear()
-        Router.replace({
-            pathname: '/',
-        },
-            undefined, { shallow: true }
-        )
-        childToParent(false)
+  useEffect(() => {
+    const storedSearchResults = sessionStorage.getItem('profileSearchResults');
+    if (storedSearchResults) {
+      setStoredSearchResults(JSON.parse(storedSearchResults));
     }
-    return (
-        <div className={styles.container}>
-
-            <div className={styles.words}>
-                <a className={styles.searchResults}>
-                    Search Results
-                </a>
-                <div data-testid="clear"
-                    className={styles.clearButton} onClick={ClearSearchResults}> <XMarkIcon className={styles.xicon} /> <a className={styles.clearWords}>Clear Results</a></div>
-            </div>
-            {storedSearchResults && storedSearchResults?.map((profile: BasicProfile, idx: React.Key) => {
-                return (
-                    <div className={styles.searchcontain} key={idx}>
-                        <SearchResultBox data={profile} />
-                    </div>)
-            })}
+  }, []);
+  const ClearSearchResults = () => {
+    sessionStorage.clear();
+    Router.replace(
+      {
+        pathname: '/',
+      },
+      undefined,
+      { shallow: true }
+    );
+    childToParent(false);
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.words}>
+        <a className={styles.searchResults}>Search Results</a>
+        <div
+          data-testid='clear'
+          className={styles.clearButton}
+          onClick={ClearSearchResults}
+        >
+          {' '}
+          <XMarkIcon className={styles.xicon} />{' '}
+          <a className={styles.clearWords}>Clear Results</a>
         </div>
-    )
-}
+      </div>
+      {storedSearchResults &&
+        storedSearchResults?.map((profile: BasicProfile, idx: React.Key) => {
+          return (
+            <div className={styles.searchContain} key={idx}>
+              <SearchResultBox data={profile} />
+            </div>
+          );
+        })}
+    </div>
+  );
+};
 
-
-export default SearchResult
+export default SearchResult;
